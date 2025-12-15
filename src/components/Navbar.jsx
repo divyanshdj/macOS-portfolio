d9e62b52f08f83d7c67b64435c51b67e8db729a3
Divyansh Jain <djboss8834@gmail.com>
2025-12-16 00:40:18 +0530

    feat: improve navbar and new wallpaper added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,35 @@
 import dayjs from "dayjs";
 import { navIcons, navLinks } from "../constants";
 import useWindowStore from "../store/window";
+import { Battery, Signal, Wifi } from "lucide-react";
 
 const Navbar = () => {
-
   const { openWindow } = useWindowStore();
   return (
-    <nav>
-      <div>
+    <nav className="sm:relative">
+      <time className="sm:hidden text-sm">{dayjs().format("h:mm")}</time>
+
+      <div className="absolute left-1/2 -translate-x-1/2 sm:static sm:translate-x-0 flex items-center gap-3">
         <img src="/images/logo.svg" alt="apple logo" />
-        <p className="font-bold">Divyansh's Portfolio</p>
-        <ul>
+        <p className="font-bold text-sm sm:text-base">Divyansh's Portfolio</p>
+
+        <ul className="hidden sm:flex">
           {navLinks.map(({ id, name, type }) => (
-            <li key={id} onClick={()=>openWindow(type)}>
+            <li key={id} onClick={() => openWindow(type)}>
               <p>{name}</p>
             </li>
           ))}
         </ul>
       </div>
 
-      <div>
-        <ul>
+      <div className="flex justify-end sm:hidden gap-1">
+        <Signal className="icon" strokeWidth={2.5} />
+        <Wifi className="icon" strokeWidth={2.5} />
+        <Battery className="icon" strokeWidth={2.5} fill="currentColor" />
+      </div>
+
+      <div className="hidden sm:flex items-center gap-5">
+        <ul className="flex items-center gap-5">
           {navIcons.map(({ id, img }) => (
             <li key={id}>
               <img src={img} className="icon-hover" alt={`img-${id}`} />
