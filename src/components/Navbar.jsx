865dd876b8db8a20dcc4bdf38e36b4846276f6f3
Divyansh Jain <djboss8834@gmail.com>
2025-12-16 01:10:33 +0530

    style: mobile dynamic island navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,12 @@ const Navbar = () => {
     <nav className="sm:relative">
       <time className="sm:hidden font-semibold text-sm">{dayjs().format("h:mm A")}</time>
 
-      <div className="absolute left-1/2 -translate-x-1/2 sm:static sm:translate-x-0">
+      <div className="sm:hidden absolute left-1/2 -translate-x-1/2 w-3/12 py-3.5 rounded-full bg-black/90">
+      <span className="absolute right-3 w-3 h-3 z-0 rounded-full bg-gray-400/10"/>
+      <span className="absolute right-4 h-1 w-1 rounded-full z-10 bg-gray-600/10" />
+      </div>
+
+      <div className="max-sm:hidden">
         <img src="/images/logo.svg" alt="apple logo" />
         <p className="font-bold text-sm sm:text-base">Divyansh's Portfolio</p>
 
